3c92e55607d1410e0dd71ddc03ab7869ce7c19af
Moshiur-15 <masiurislam28@gmail.com>
2025-01-08 22:55:23 +0600

    update review page dark theme apply

--- game-review-client/src/Components/UpdateReview.jsx
+++ game-review-client/src/Components/UpdateReview.jsx
@@ -53,14 +53,15 @@ export default function UpdateReview() {
       });
   };
   return (
-    <div className="max-w-3xl my-10 mx-auto p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
+    <section className="dark:bg-slate-900 py-10">
+      <div className="max-w-3xl mx-auto p-6 bg-gray-200/80 dark:bg-slate-700 rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold text-center text-gray-800 dark:text-white mb-6">
         Update Review
       </h2>
       <form onSubmit={handleUpdate} className="space-y-4">
         <div className="sm:flex gap-2">
           <div className="w-full">
-            <label className="block text-sm font-medium text-gray-700">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
               Game Cover URL
             </label>
             <input
@@ -69,13 +70,13 @@ export default function UpdateReview() {
               name="coverUrl"
               placeholder="https://example.com/cover.jpg"
               required
-              className="input border-green-600 w-full"
+              className="input w-full focus:outline-none dark:bg-slate-800 dark:text-slate-400"
             />
           </div>
           <div className="w-full">
             <label
               htmlFor="genre"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
             >
               Genre
             </label>
@@ -83,7 +84,7 @@ export default function UpdateReview() {
               name="genre"
               defaultValue={genre}
               required
-              className="input border-green-600 w-full"
+              className="input w-full focus:outline-none dark:bg-slate-800 dark:text-slate-400"
             >
               <option disabled selected>
                 Select Genre
@@ -104,7 +105,7 @@ export default function UpdateReview() {
           <div className="w-full">
             <label
               htmlFor="gameTitle"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
             >
               Game Title
             </label>
@@ -114,12 +115,12 @@ export default function UpdateReview() {
               name="gameTitle"
               placeholder="Enter game title"
               required
-              className="input border-green-600 w-full"
+              className="input w-full focus:outline-none dark:bg-slate-800 dark:text-slate-400"
             />
           </div>
 
           <div className="w-full">
-            <label className="block text-sm font-medium text-gray-700">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
               Rating (1-5)
             </label>
             <input
@@ -129,13 +130,13 @@ export default function UpdateReview() {
               min="1"
               max="5"
               required
-              className="input border-green-600 w-full"
+              className="input w-full focus:outline-none dark:bg-slate-800 dark:text-slate-400"
             />
           </div>
         </div>
 
         <div>
-          <label className="block text-sm font-medium text-gray-700">
+          <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
             Review Description
           </label>
           <textarea
@@ -144,12 +145,12 @@ export default function UpdateReview() {
             rows="5"
             placeholder="Write your review here"
             required
-            className="textarea border-green-600 w-full"
+            className="textarea w-full focus:outline-none dark:bg-slate-800 dark:text-slate-400"
           ></textarea>
         </div>
 
         <div>
-          <label className="block text-sm font-medium text-gray-700">
+          <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
             User Name
           </label>
           <input
@@ -157,12 +158,12 @@ export default function UpdateReview() {
             name="userName"
             defaultValue={userName}
             readOnly
-            className="input border-green-600 w-full"
+            className="input w-full focus:outline-none dark:bg-slate-800 dark:text-slate-400"
           />
         </div>
 
         <div>
-          <label className="block text-sm font-medium text-gray-700">
+          <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
             User Email
           </label>
           <input
@@ -170,19 +171,20 @@ export default function UpdateReview() {
             type="email"
             name="userEmail"
             defaultValue={userEmail}
-            className="input border-green-600 w-full"
+            className="input w-full focus:outline-none dark:bg-slate-800 dark:text-slate-400"
           />
         </div>
 
         <div className="text-center">
           <button
             type="submit"
-            className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold btn"
+            className="w-full text-white font-semibold btn bg-blue-500 hover:bg-blue-600 border-none"
           >
             Submit Review
           </button>
         </div>
       </form>
     </div>
+    </section>
   );
 }
